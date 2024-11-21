c6c31bba1dadbdf29e0c4a97e6aedd0125fd17fc
ihebmeftah <ihebmeftah666@gmail.com>
2024-11-21 19:38:49 +0100

    Add JPA annotations to User model
    Enhance the User class by adding JPA annotations to make it a JPA entity. This includes annotations for defining the table, primary key, and unique constraints.

--- src/main/java/com/project/eventsapp/web/models/auth/User.java
+++ src/main/java/com/project/eventsapp/web/models/auth/User.java
@@ -6,14 +6,25 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
+    @Column(unique = true)
     private String email;
     private String password;
     private roles role;
