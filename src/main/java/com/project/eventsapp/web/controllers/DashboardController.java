fb45fb5e7d58bbd303c47b88a74d6af0d63ba677
ihebmeftah <ihebmeftah666@gmail.com>
2024-12-18 02:11:40 +0100

    Add event count to dashboard
    Updated the DashboardController to fetch the total number of events from the EventsServices and added this count to the model. Modified the dashboard.html to display the number of events created using Thymeleaf.

--- src/main/java/com/project/eventsapp/web/controllers/DashboardController.java
+++ src/main/java/com/project/eventsapp/web/controllers/DashboardController.java
@@ -1,13 +1,21 @@
 package com.project.eventsapp.web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.project.eventsapp.business.services.EventsServices;
+
 @Controller
 public class DashboardController {
+    @Autowired
+    private EventsServices eventsServices;
+
     @GetMapping("dashboard")
     public String getDashboard(Model model) {
+        Long total = eventsServices.getNbEvents();
+        model.addAttribute("eventsTotal", total);
         return "dashboard";
     }
 }
